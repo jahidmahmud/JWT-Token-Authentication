6c2c10bda924663d9f7407cb6352472b84df4feb
jahidmahmud <mahmud.jahid519@gmail.com>
2021-07-05 11:59:34 +0600

    Init

---
+++ JWT_Authentication/Controllers/NameController.cs
@@ -0,0 +1,49 @@
+﻿using JWT_Authentication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JWT_Authentication.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly IJwtAuthenticationManager manager;
+
+        public NameController(IJwtAuthenticationManager manager)
+        {
+            this.manager = manager;
+        }
+        // GET: api/<NameController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<NameController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        [HttpPost("Authenticate")]
+        [AllowAnonymous]
+        public IActionResult Authenticate([FromBody]UserCredential user)
+        {
+            var token=manager.Authenticate(user.UserName, user.Password);
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(token);
+        }
+    }
+}
